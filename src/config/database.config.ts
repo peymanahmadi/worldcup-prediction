88ec8f2424e917c3bc6a34db06ccd6423f9401f3
peymanahmadi <peyman.ahmadi@outlook.com>
2025-11-02 09:54:26 +0330

    feat: Create user entity and synchronize db config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -8,8 +8,10 @@ export default registerAs(
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432', 10),
     username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
+    password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'worldcup',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: process.env.NODE_ENV === 'development',
+    logging: process.env.NODE_ENV === 'development',
   }),
 );
---
+++ src/database/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 11, unique: true })
+  phone: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  password_hash: string;
+
+  @Column({ type: 'boolean', default: true })
+  is_active: boolean;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updated_at: Date;
+}
