d50ed777829c977e4eedb3bd97b7325402f12812
peymanahmadi <peyman.ahmadi@outlook.com>
2025-11-04 23:34:25 +0330

    feat(healthCheck): Implement healthCheck

---
+++ src/modules/health/health.controller.ts
@@ -0,0 +1,25 @@
+import { Public } from '@common/decorators/public.decorator';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+
+@ApiTags('health')
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Get()
+  @Public()
+  @HealthCheck()
+  @ApiOperation({ summary: 'Health check endpoint' })
+  check() {
+    return this.health.check([() => this.db.pingCheck('database')]);
+  }
+}
